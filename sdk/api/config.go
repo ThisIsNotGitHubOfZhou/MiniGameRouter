d9fdc3add0d2230640de449053ba4c89aa76b0c5
thisiszhou <thisiszhou@tencent.com>
2024-07-29 16:26:51 +0800

    修改端口名

--- sdk/api/config.go
+++ sdk/api/config.go
@@ -14,9 +14,9 @@ var (
 
 func init() {
 	// 定义命令行标志
-	flag.StringVar(&RegisteGrpcrHost, "host", "9.135.95.71", "The host to register grpc")
-	flag.StringVar(&RegisterGrpcPort, "port", "20001", "The port to register grpc")
-	flag.StringVar(&HealthCheckGrpcHost, "host", "9.135.95.71", "The host to register grpc")
-	flag.StringVar(&HealthCheckGrpcPort, "port", "30001", "The port to register grpc")
+	flag.StringVar(&RegisteGrpcrHost, "registerhost", "9.135.95.71", "The host to register grpc")
+	flag.StringVar(&RegisterGrpcPort, "registerport", "20001", "The port to register grpc")
+	flag.StringVar(&HealthCheckGrpcHost, "healthcheckhost", "9.135.95.71", "The host to register grpc")
+	flag.StringVar(&HealthCheckGrpcPort, "healthcheckport", "30001", "The port to register grpc")
 	flag.BoolVar(&IsK8s, "k8s", false, "Is running in Kubernetes")
 }
